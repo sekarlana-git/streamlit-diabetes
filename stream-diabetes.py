47ed7198aa92e176e1c2e86db6d04601f042bec3
sekarlana-git <lanabelajar07@gmail.com>
2025-05-07 22:49:04 +0800

    Add files via upload

---
+++ stream-diabetes.py
@@ -0,0 +1,49 @@
+import pickle
+import streamlit as st
+
+#membaca model
+diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
+
+#judul web
+st.title('Data Mining Prediksi Diabetes')
+
+#membagi kolom
+col1, col2 = st.columns(2)
+
+with col1 :
+    Pregnancies = st.text_input ('input nilai Pregnancies')
+
+with col2 :
+    Glucose = st.text_input ('input nilai Glucose')
+
+with col1 :
+    BloodPressure = st.text_input ('input nilai Blood Pressure')
+
+with col2 :
+    SkinThickness = st.text_input ('input nilai SkinThickness')
+
+with col1 :
+    Insulin = st.text_input ('input nilai Insulin')
+
+with col2 :
+    BMI = st.text_input ('input nilai BMI')
+
+with col1 :
+    DiabetesPedigreeFunction = st.text_input ('input nilai DiabetesPedigreeFunction')
+
+with col2 :
+    Age = st.text_input ('input nilai Age')
+
+#kode untuk prediksi
+diab_diagnosis = ''
+
+#membuat tombol untuk prediksi
+
+if st.button('Test Prediksi Diabetes'):
+    diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+
+    if(diab_prediction[0] == 1):
+        diab_diagnosis = 'Pasien Terkena Diabetes'
+    else :
+        diab_diagnosis = 'Pasien Tidak Terkena Diabetes'
+    st.success(diab_diagnosis)
