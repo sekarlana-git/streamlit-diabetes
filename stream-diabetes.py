741abc0b68118aa4ae9418fe2114282a2728fc0e
sekarlana-git <lanabelajar07@gmail.com>
2025-05-07 23:27:51 +0800

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 
 #membaca model
-diabetes_model = pickle.load(open('diabetes_model.sav'))
+diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
 
 #judul web
 st.title('Data Mining Prediksi Diabetes')
